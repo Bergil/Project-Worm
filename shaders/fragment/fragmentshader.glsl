0c674fa141a96badca4f8ee8eba985f23680e6d7
Senryoku <maretverdant@gmail.com>
2017-02-03 19:40:34 +0100

    Fix shader compilation on Nvidia

--- shaders/fragment/fragmentshader.glsl
+++ shaders/fragment/fragmentshader.glsl
@@ -1,3 +1,5 @@
+#version 430 core
+
 in vec3 normale;
 in vec3 position;
 out vec4 colorOut;
@@ -5,7 +7,7 @@ out vec4 colorOut;
 void main(){
 
 	//colorOut = vec4(position, 1.0);
-	colorOut.rgb = vec3(1, 0, 0) * clamp(dot(normale, vec3(1.0,0.0,0.0)), 0.2, 1);
+	colorOut.rgb = vec3(1, 0, 0) * clamp(dot(normale, vec3(1.0,0.0,0.0)), 0.2, 1.0);
 	colorOut.a = 1.0;
 	
 }
